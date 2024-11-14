40d887ae991e2fb9f89fd04064b225794bd13c4e
Pramod Gupta <51231047+FGpramodgupta@users.noreply.github.com>
2024-11-14 13:37:50 +0530

    Update html2pdf.py

--- html2pdf.py
+++ html2pdf.py
@@ -1,1 +1,21 @@
 
+import re
+
+# Sample HTML content
+html_content = '''
+<html>
+<body>
+    <img src="image1.jpg" alt="First image">
+    <p>Some text here.</p>
+    <img src="image2.png" alt="Second image">
+</body>
+</html>
+'''
+
+# Regex pattern to find <img> tags with src attributes
+pattern = r'(<img\s[^>]*src=)"[^"]*"'
+
+# Replace with an empty src attribute
+updated_html = re.sub(pattern, r'\1""', html_content)
+
+print(updated_html)
